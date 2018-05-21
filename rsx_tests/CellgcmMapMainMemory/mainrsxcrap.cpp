049dff16ccbdd22df6ca760e3b10599279173f62
eladash <elad3356p@gmail.com>
2018-05-21 22:31:08 +0300

    cellgcm stuff

---
+++ rsx_tests/CellgcmMapMainMemory/mainrsxcrap.cpp
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/process.h>
+#include <sys/synchronization.h>
+#include <sys/prx.h>
+
+#include <sysutil/sysutil_syscache.h>
+#include <cell/sysmodule.h>
+#include <sys/ppu_thread.h>
+#include <sys/timer.h>
+#include <sys/memory.h>
+#include <sys/event.h>
+#include <sys/syscall.h>
+#include <cell/gcm.h>
+#include <memory>
+
+typedef uintptr_t uptr;
+typedef uint64_t u64;
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t u8;
+
+#define int_cast(addr) reinterpret_cast<uintptr_t>(addr)
+#define ptr_cast(intnum) reinterpret_cast<void*>(intnum)
+
+enum {nullptr = 0};
+
+// Set priority and stack size for the primary PPU thread.
+// Priority : 1000
+// Stack    : 64KB
+SYS_PROCESS_PARAM(1000, 0x10000)
+
+sys_memory_t mem_id;
+sys_addr_t addr1;
+sys_addr_t addr2;
+sys_event_queue_t queue_id;
+
+int main() {
+
+	register int ret asm ("3");
+	{
+		u64 addr;
+		system_call_3(SYS_RSX_DEVICE_MAP, int_cast(&addr), nullptr, 8);
+
+		printf("ret is 0x%x, addr = 0x%x\n", ret, addr);
+	}
+
+	u32 mem_handle;
+	u64 mem_addr; 
+	{
+		system_call_7(SYS_RSX_MEMORY_ALLOCATE, int_cast(&mem_handle), int_cast(&mem_addr), 0xf900000, 0x80000, 0x300000, 0xf, 0x8); //size = 0xf900000, 249 mb
+
+		printf("ret is 0x%x, mem_handle=0x%x, mem_addr=0x%x\n", ret, mem_handle, mem_addr);
+	}
+
+	u32 context_id;
+	u64 lpar_dma_control, lpar_driver_info, lpar_reports;
+	{
+		system_call_6(SYS_RSX_CONTEXT_ALLOCATE, int_cast(&context_id), int_cast(&lpar_dma_control), int_cast(&lpar_driver_info), int_cast(&lpar_reports), mem_handle /*0x5a5a5a5b*/, 0x820);
+
+		printf("ret is 0x%x, context_id=0x%x, lpar_dma_control=0x%x, lpar_driver_info=0x%x, lpar_reports=0x%x\n", ret, context_id, lpar_dma_control, lpar_driver_info, lpar_reports);
+	}
+
+	sys_mmapper_allocate_memory(0x800000, SYS_MEMORY_GRANULARITY_1M, &mem_id);
+
+	printf("ret is 0x%x, mem_id=0x%x\n", ret, mem_id);
+
+	sys_mmapper_allocate_address(0x10000000, 0x40f, 0x10000000, &addr1);
+
+	printf("ret is 0x%x, addr1=0x%x\n", ret, addr1);
+
+	sys_mmapper_map_memory(u64(addr1), mem_id, SYS_MEMORY_PROT_READ_WRITE);
+
+	printf("ret is 0x%x\n", ret);
+
+	u32 offset;
+
+	ret = cellGcmMapMainMemory(ptr_cast(addr1), 2u << 20, &offset);
+
+	printf("ret is 0x%x, offset=0x%x\n", ret, offset);
+
+	ret = cellGcmMapMainMemory(ptr_cast(addr1 + (1u << 20)), 1u << 20, &offset);
+
+	printf("ret is 0x%x, offset=0x%x\n", ret, offset);
+
+	
+    printf("sample finished.\n");
+
+    return 0;
+}
