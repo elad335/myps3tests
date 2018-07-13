7f6d458e99d63b56ea90aa82b99344da6a99b86b
eladash <elad3356p@gmail.com>
2018-07-13 14:52:20 +0300

    atomic commands address test and cleanup

--- spu_tests/atomic commands address alignment/test_runner.spu.cpp
+++ spu_tests/atomic commands address alignment/test_runner.spu.cpp
@@ -14,36 +14,22 @@ typedef uint8_t u8;
 #define ptr_dref(x, T) *reinterpret_cast<T*>(x)
 #define int_cast(x) reinterpret_cast<u32>(x)
 
-inline void mfence() { asm volatile ("lnop;syncc;sync;dsync;syncc;sync;dsync;syncc;sync;dsync"); };
+inline void mfence() { asm volatile ("syncc;sync;dsync"); };
 static u64 rdata[16] __attribute__((aligned(256))) = {};
 
 int main(u64 raddr, u64, u64, u64) {
 
-    spu_printf("0x%x\n", spu_readch(MFC_EAL));
-    spu_printf("0x%x\n", spu_readch(MFC_EAH));
-    spu_printf("0x%x\n", spu_readch(MFC_Size));
-    spu_printf("0x%x\n", spu_readch(MFC_TagID));
-    spu_printf("0x%x\n", spu_readch(MFC_LSA));
-    spu_readch(127);
     spu_printf("0x%x\n", raddr);
-    mfence();
-
-    spu_writech(SPU_WrEventMask, 0x400); // LR event
-    mfence();
-    if (spu_readchcnt(SPU_RdEventStat)) 
-    {
-        spu_writech(SPU_WrEventAck, spu_readch(SPU_RdEventStat)); // if count is 1, make it 0 and ack existing events
-    }
     mfence();
     spu_printf("ok\n");
 
-    spu_writech(MFC_EAL, raddr + 0x11); // Unaligned!!
+    spu_writech(MFC_EAL, raddr + 0x7F); // Unaligned!!
     spu_writech(MFC_EAH, 0);
-    spu_writech(MFC_LSA , int_cast(&rdata[0]) + 0x11); // Unaligned!!
+    spu_writech(MFC_LSA , int_cast(&rdata[0]) + 0x7F); // Unaligned!!
     spu_writech(MFC_Size, 0x80);
     spu_writech(MFC_Cmd, 0xD0); // GETLLAR
     mfence(); spu_readch(MFC_RdAtomicStat); mfence();
-    mfence();mfence();mfence();mfence();mfence();mfence();
+
     spu_printf("rdata:\n"
     "%llx\n"
     "%llx\n"
@@ -83,7 +69,7 @@ int main(u64 raddr, u64, u64, u64) {
     spu_writech(MFC_EAH, 0);
     spu_writech(MFC_LSA , int_cast(&rdata[0])); // aligned
     spu_writech(MFC_Size, 0x80);
-    spu_writech(MFC_Cmd, 0xB4); // GETLLAR
+    spu_writech(MFC_Cmd, 0xB4); // PUTLLC
     mfence(); spu_printf("reservation status: 0x%x", spu_readch(MFC_RdAtomicStat)); mfence();
 
     sys_spu_thread_exit(0);
