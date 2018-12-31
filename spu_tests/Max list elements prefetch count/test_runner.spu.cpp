864d4084c4397ab63676619b9b008f8b3ddfdbde
elad <elad3356p@gmail.com>
2018-12-31 06:23:33 +0200

    stuff

---
+++ spu_tests/Max list elements prefetch count/test_runner.spu.cpp
@@ -0,0 +1,33 @@
+#include <sys/spu_thread.h>	
+#include <spu_printf.h>
+#include <spu_intrinsics.h>
+#include <spu_internals.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "../spu_header.h"
+
+static mfc_list_element_t mfcData[16] __attribute__((aligned(128)));
+
+int main(u64 addr) {
+
+    mfc_fence();
+
+    // The first elements fetches 128 bytes on list
+    mfcData[0].size = 0x80;
+    mfcData[0].eal = addr;
+    mfcData[0].notify = 0;
+
+    // Fill the rest of the elements with NOP transfers
+    for (u32 i = 1; i < 16; i++)
+    {
+        mfcData[i].size = 0;
+        mfcData[i].notify = 0;
+        mfcData[i].eal = addr;
+    }
+
+    mfc_getl(&mfcData[0], 0, &mfcData[0], 0x30, 0, 0,0);
+    mfc_fence();
+    sys_spu_thread_exit(0);
+	return 0;
+}
