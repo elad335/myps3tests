75a99c51f398d2b07aee0e5e415db11f11490097
eladash <elad3356p@gmail.com>
2019-04-05 11:48:39 +0300

    update

--- ppu_tests/cellSaveDataUserAutoLoad/ppumain.cpp
+++ ppu_tests/cellSaveDataUserAutoLoad/ppumain.cpp
@@ -8,6 +8,7 @@
 
 #include <sysutil/sysutil_savedata.h> 
 #include <sysutil/sysutil_syscache.h>
+#include <sysutil/sysutil_userinfo.h> 
 #include <cell/sysmodule.h>
 #include <sys/ppu_thread.h>
 #include <sys/timer.h>
@@ -26,16 +27,26 @@
 SYS_PROCESS_PARAM(1000, 0x100000)
 
 CellSaveDataAutoIndicator ind;
+static u32 iteration = 0;
 
 void funcStat(CellSaveDataCBResult *cbResult, CellSaveDataStatGet *get, CellSaveDataStatSet *set)
 {
-	printf("FuncStat() called! bind=0x%x, sizeKB=0x%x, \n", get->bind, get->sizeKB);
+	printf("FuncStat() called! bind=0x%x, sizeKB=0x%x, newData=%d, hddFree=0x%x\n", get->bind, get->sizeKB, get->isNewData
+	, get->hddFreeSizeKB);
+
 	cbResult->result = 0;
-	set->indicator = &ind;
-	set->setParam = &get->getParam;
-	//set->reCreateMode = CELL_SAVEDATA_RECREATE_NO_NOBROKEN; // Error
-	set->reCreateMode = CELL_SAVEDATA_RECREATE_YES;
-	//set->reCreateMode = CELL_SAVEDATA_RECREATE_YES | (1<<31); // Error
+
+	set->setParam = iteration == 1 || iteration == 3 ? &get->getParam : NULL;
+	set->reCreateMode =  iteration >= 2 ? CELL_SAVEDATA_RECREATE_YES : CELL_SAVEDATA_RECREATE_NO_NOBROKEN;
+
+	switch (iteration)
+	{
+	case 0: printf("Using CELL_SAVEDATA_RECREATE_NO_NOBROKEN, setParam is NULL, directory doesnt exists\n"); break;
+	case 1: printf("Using CELL_SAVEDATA_RECREATE_NO_NOBROKEN, setParam is set, directory doesnt exists\n"); break;
+	case 2: printf("Using CELL_SAVEDATA_RECREATE_YES, setParam is NULL\n"); break;
+	case 3: printf("Using CELL_SAVEDATA_RECREATE_YES, setParam is set, checking stats"); break;
+	}
+
 	//set->setParam->reserved2[0] = (char)0xff; // Error
 	//set->setParam->attribute = -1; // Invalid, error
 	//set->setParam->reserved[0] = (char)0xff; error
@@ -44,13 +55,14 @@ void funcStat(CellSaveDataCBResult *cbResult, CellSaveDataStatGet *get, CellSave
 void funcFile(CellSaveDataCBResult *cbResult, CellSaveDataFileGet *get, CellSaveDataFileSet *set)
 {
 	printf("FuncFile() called!\n");
-	cbResult->result = 1; // Abort prematuraly
+	cbResult->result = 1;
 } 
 
 int main() {
 
 	cellSysmoduleLoadModule( CELL_SYSMODULE_SYSUTIL );
 	cellSysmoduleLoadModule(CELL_SYSMODULE_SYSUTIL_SAVEDATA);
+
 	int ret = 0;
 	CellSaveDataSetBuf setBuf = {
 	1, // dirListMax;
@@ -60,9 +72,21 @@ int main() {
 	NULL, // buf
 	};
 
-	setBuf.buf = (void*)(new u8[setBuf.bufSize]);
+	setBuf.buf = malloc(setBuf.bufSize);
 
-	ret = cellSaveDataUserAutoLoad(0, 0, "BLES00412", 0, &setBuf, funcStat, funcFile, 0xffffffff, NULL);
-	printf("ret = 0x%x", ret);
+	ret = cellSaveDataUserAutoLoad(0, 0, "BLUS12345", 0, &setBuf, funcStat, funcFile, 0xffffffff, NULL);
+	printf("ret = 0x%x\n", ret);
+	iteration++;
+	sys_timer_usleep(500);
+	ret = cellSaveDataUserAutoLoad(0, 0, "BLUS12345", 0, &setBuf, funcStat, funcFile, 0xffffffff, NULL);
+	printf("ret = 0x%x\n", ret);
+	iteration++;
+	sys_timer_usleep(500);
+	ret = cellSaveDataUserAutoLoad(0, 0, "BLUS12345", 0, &setBuf, funcStat, funcFile, 0xffffffff, NULL);
+	printf("ret = 0x%x\n", ret);
+	iteration++;
+	sys_timer_usleep(500);
+	ret = cellSaveDataUserAutoLoad(0, 0, "BLUS12345", 0, &setBuf, funcStat, funcFile, 0xffffffff, NULL);
+	printf("ret = 0x%x\n", ret);
  	return 0;
 }
