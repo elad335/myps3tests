fbf1c2b5395910b1a272fd68367b76c2df06dd65
Eladash <elad3356p@gmail.com>
2020-01-04 11:12:38 +0200

    update

--- ppu_tests/cellFsGetDirectoryEntries/ppumain.cpp
+++ ppu_tests/cellFsGetDirectoryEntries/ppumain.cpp
@@ -27,7 +27,6 @@
 // Set priority and stack size for the primary PPU thread.
 // Priority : 1000
 // Stack    : 64KB
-// Allows for cache id to be 32 characters long (not including null term)
 SYS_PROCESS_PARAM(1000, 0x10000)
 
 void strcpy_trunc(char* dst, const char* src, size_t max_count)
@@ -46,57 +45,90 @@ void CacheMount(CellSysCacheParam& _cache)
 
 #define cache_id  "16-45-01-83"
 
-void printBytes(void* data, size_t size)
+void printBytes(const void* data, size_t size)
 {
-	for (u32 i = 0; i < size; i++)
+	const u8* bytes = static_cast<const u8*>(data);
+
+	for (u32 i = 0; i < size;)
 	{
-		printf("[%04x] %02X\n", i, static_cast<u8*>(data)[i]);
+		const u32 read = std::min<u32>(size - i, 4);
+		printf("[%04x]", i);
+
+		switch (size - i)
+		{
+		default: printf(" %02X", bytes[i++]);
+		case 3: printf(" %02X", bytes[i++]);
+		case 2: printf(" %02X", bytes[i++]);
+		case 1: printf(" %02X", bytes[i++]);
+		}
+
+		printf("\n");
 	}	
 }
 
 int main() {
 
 	cellSysmoduleLoadModule( CELL_SYSMODULE_SYSUTIL );
+	cellSysmoduleLoadModule( CELL_SYSMODULE_FS );
 
 	CellSysCacheParam param = {};
 
-	strcpy_trunc(param.cacheId, cache_id, 15);
+	// Actually empty id to get it as empty dir
+	strcpy_trunc(param.cacheId, cache_id, 0);
 
 	CacheMount(param);
-	cellSysCacheClear();
 
 	std::string strtemp(param.getCachePath);
 	strtemp += "/MyDir/";
 
-	ENSURE_OK(cellFsMkdir(strtemp.c_str(), CELL_FS_DEFAULT_CREATE_MODE_3));
+	ENSURE_OK(cellFsMkdir(strtemp.c_str(), CELL_FS_DEFAULT_CREATE_MODE_1));
 
 	s32 fd = -1;
+
+	ENSURE_OK(cellFsOpen((strtemp + "txt.txt").c_str(), CELL_FS_O_CREAT, &fd, NULL, 0));
+	ENSURE_OK(cellFsClose(fd));
+
 	u64 nread = -1;
-	CellFsDirectoryEntry entry[2];
+	CellFsDirectoryEntry entry[2]; // Two entries
 	u32 count = -1;
-	std::memset(&entry, 0xff, sizeof entry);
 	ENSURE_OK(cellFsOpendir(strtemp.c_str(), &fd));
 
-	ENSURE_OK(cellFsGetDirectoryEntries(fd, entry, sizeof entry, &count));
-	printf("data_count = 0x%x\n", count);
-	printBytes(entry, sizeof entry);
-	std::memset(&entry, 0xff, sizeof entry);
-	ENSURE_OK(cellFsGetDirectoryEntries(fd, entry, sizeof entry, &count));
-	printf("data_count = 0x%x\n", count);
-	printBytes(entry, sizeof entry);
-	std::memset(&entry, 0xff, sizeof entry);
+	do
+	{
+		std::memset(&entry, 0xff, sizeof entry);
+		ENSURE_OK(cellFsGetDirectoryEntries(fd, entry, sizeof entry, &count));
+		printf("data_count = 0x%x\n", count);
+
+		for (u32 i = 0; i < 2; i++)
+		{
+			printf("entry[%d]:\n", i);
+
+			if (i < count)
+			{
+				printf("d_name = \"%s\"\n", entry[i].entry_name.d_name);
+				printf("uid=%lld, gid=%lld\n", entry[i].attribute.st_uid, entry[i].attribute.st_gid);
+			}
+
+			printBytes(entry + i, sizeof(*+entry));
+		}
+	}
+	while (count);
+
 	ENSURE_OK(cellFsClosedir(fd));
 
 	/*ENSURE_OK(cellFsOpendir(strtemp.c_str(), &fd));
 
-	ENSURE_OK(cellFsReaddir(fd, &entry[0].entry_name, &nread));
-	ENSURE_OK(cellFsReaddir(fd, &entry[0].entry_name, &nread));
-	ENSURE_OK(cellFsReaddir(fd, &entry[0].entry_name, &nread));
-	
+	do 
+	{
+		ENSURE_OK(cellFsReaddir(fd, &entry[0].entry_name, &nread));
+	}
+	while (nread);
+
 	std::memset(&entry[0].entry_name, 0xff, sizeof (entry[0].entry_name));
 	ENSURE_OK(cellFsReaddir(fd, &entry[0].entry_name, &nread));
 	printf("nread = 0x%x\n", nread);
 	printBytes(&entry[0].entry_name, sizeof (entry[0].entry_name));	
 	ENSURE_OK(cellFsClosedir(fd));*/
+
 	return 0;
 }
