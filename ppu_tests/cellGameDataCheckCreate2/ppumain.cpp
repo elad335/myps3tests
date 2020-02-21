d1809fb23efd255984ecc61e6ccc3189d89e9321
Eladash <elad3356p@gmail.com>
2020-02-21 19:24:53 +0200

    Update

--- ppu_tests/cellGameDataCheckCreate2/ppumain.cpp
+++ ppu_tests/cellGameDataCheckCreate2/ppumain.cpp
@@ -32,6 +32,8 @@
 SYS_PROCESS_PARAM(1000, 0x100000)
 
 u32 callback_count = 0;
+std::string dir;
+
 void funcStat(
  CellGameDataCBResult *cbResult,
  CellGameDataStatGet *get,
@@ -44,12 +46,17 @@ void funcStat(
 	if (callback_count)
 	{
 		set->setParam = &get->getParam;
+
+		if (dir.empty())
+		{
+			dir = get->contentInfoPath;
+		}
 	}
 
 	cbResult->result = 0;
 	return;
 }
-int main() {
+int main(int argc, char* argv[]) {
 
 	cellSysmoduleLoadModule( CELL_SYSMODULE_SYSUTIL );
 	cellSysmoduleLoadModule( CELL_SYSMODULE_SYSUTIL_NP );
@@ -70,8 +77,41 @@ int main() {
 	cellFunc(DiscGameGetBootDiscInfo, getParam );
 	printBytes(getParam, sizeof getParam);
 
-	for (;callback_count < 3; callback_count++) cellFunc(GameDataCheckCreate, 0, "NPEB10097", 0, funcStat, container);
+	int sfo, to_sfo;
+	std::string strpath (argv[0]);
+	strpath = strpath.substr(0, strpath.find_last_of('/'));
 
+	ENSURE_OK(cellFsOpen((strpath + "/PARAM2.SFO").c_str(), 0, &sfo, NULL, 0));
+
+	CellFsStat stat;
+	ENSURE_OK(cellFsFstat(sfo, &stat));
+
+	void* sfo_buf = malloc(stat.st_size);
+
+	ENSURE_OK(cellFsRead(sfo, sfo_buf, stat.st_size, &stat.st_size));
+	
+	ENSURE_OK(cellFsClose(sfo));
+
+	for (callback_count = 0;callback_count < 3; callback_count++)
+	{
+		cellFunc(GameDataCheckCreate, 0, "NPEB21201", 0, funcStat, container);
+	}
+
+	for (callback_count = 0; callback_count < 1; callback_count++)
+	{
+		if (!dir.empty()) cellFunc(FsUnlink, (dir + "/PARAM.SFO").c_str());
+		cellFunc(GameDataCheckCreate, 0, "NPEB21201", 0, funcStat, container);
+	}
+
+	ENSURE_OK(cellFsOpen((dir + "/PARAM.SFO").c_str(), CELL_FS_O_CREAT | CELL_FS_O_RDWR | CELL_FS_O_EXCL, &to_sfo, NULL, 0));
+	ENSURE_OK(cellFsWrite(to_sfo, sfo_buf, stat.st_size, &stat.st_size));
+	ENSURE_OK(cellFsFsync(to_sfo));
+	ENSURE_OK(cellFsClose(to_sfo));
+
+	for (callback_count = 0; callback_count < 1; callback_count++)
+	{
+		cellFunc(GameDataCheckCreate, 0, "NPEB21201", 0, funcStat, container);
+	}
 
 	return 0;
 }
