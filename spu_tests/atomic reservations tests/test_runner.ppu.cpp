47903795b5c7d22b4b0914028e9b228adebcfe4e
Eladash <elad3356p@gmail.com>
2020-05-01 20:19:48 +0300

    Extend reservations tests

--- spu_tests/atomic reservations tests/test_runner.ppu.cpp
+++ spu_tests/atomic reservations tests/test_runner.ppu.cpp
@@ -17,44 +17,176 @@
 /* embedded SPU ELF symbols */
 extern char _binary_test_spu_spu_out_start[];
 
-static u32 rdata[256] __attribute__((aligned(128))) = {0};
+static u8 rdata[1024] ALIGN(128) = {0};
 
-#define BEGIN_SCOPE ({
-#define CLOSE_SCOPE(expr) })
-
-static void ppu_reservation_tests()
+struct thread_data_t
 {
-	store_vol(rdata[0], 0);
-	store_vol(rdata[1], 1);
+	u32 signal;
+	void(*cb)(thread_data_t&);
+	u32 exit;
+	u32 addr;
+};
 
-	BEGIN_SCOPE;
-	__lwarx(&rdata[0]);
-	bool success = __stwcx(&rdata[1], load_vol(rdata[1])) != 0;
-	printf("PPU - Performing atomic store on a nieghbor address of load's\n status:%s\n", success ? "true" : "false");
-	CLOSE_SCOPE();
+// Thread which touches reservation data
+void helper_thread(u64 addr)
+{
+	thread_data_t& data = *ptr_caste(addr, thread_data_t);
 
-	BEGIN_SCOPE;
-	__ldarx(ptr_caste(&rdata[0], u64));
-	bool success = __stwcx(&rdata[0], load_vol(rdata[0])) != 0;
-	printf("PPU - Performing u64 to u32 atomic op\n status:%s\n", success ? "true" : "false");
-	CLOSE_SCOPE();
+	while (true)
+	{
+		while (!load_vol(data.signal))
+		{
+			sys_ppu_thread_yield();
+		}
 
-	BEGIN_SCOPE;
-	__lwarx(&rdata[0]);
-	bool success = __stdcx(ptr_caste(&rdata[0], u64), load_vol(*ptr_caste(&rdata[0], u64))) != 0;
-	printf("PPU - Performing u32 to u64 atomic op\n status:%s\n", success ? "true" : "false");
-	CLOSE_SCOPE();
+		if (load_vol(data.exit))
+		{
+			break;
+		}
 
-	BEGIN_SCOPE;
-	const u32 old = __lwarx(&rdata[0]);
-	store_vol(rdata[0], old + 1);
-	bool success = __stwcx(&rdata[0], load_vol(rdata[0])) != 0;
-	printf("PPU - Performing atomic store after a self written non-atomic store\n status:%s\n", success ? "true" : "false");
-	store_vol(rdata[0], old - 1);
-	CLOSE_SCOPE();
+		load_vol(data.cb)(data);
+		store_vol(data.signal, 0);
+	}
 
-	store_vol(rdata[0], 0);
-	store_vol(rdata[1], 0);
+	thread_exit(0);
+}
+
+// Execute callback on the other thread
+void wrap_callback(thread_data_t& data, void(*cb)(thread_data_t&))
+{
+	store_vol(data.cb, cb);
+	store_vol(data.signal, 1);
+
+	while (load_vol(data.signal)) __db16cyc();
+}
+
+// Callbacks executed on two different ppu threads
+
+// Change reservation address within the same cache line
+void change_addr(thread_data_t& data)
+{
+	store_vol(data.addr, load_vol(data.addr) ^ 0x78);
+}
+
+// Write value to the reservation cache line from different address
+void write_val(thread_data_t& data)
+{
+	store_vol(*ptr_caste(load_vol(data.addr) ^ 0x78, u32), 0); 
+}
+
+// Execut full atomic op once on reservation cache line from different address
+void atomic_op(thread_data_t& data)
+{
+	void* ptr = ptr_cast(load_vol(data.addr) ^ 0x78);
+	u32 old;
+
+	do
+	{
+		old = __lwarx(ptr);
+	}
+	while (0 == __stwcx(ptr, old));
+}
+
+// Execute a single store reservation instruction
+void store_cond(thread_data_t& data)
+{
+	u32* ptr = ptr_caste(load_vol(data.addr) ^ 0x78, u32);
+	__stwcx(ptr, load_vol(*ptr)); 
+}
+
+void invalidate_cache(thread_data_t& data)
+{
+	void* rdata = ptr_cast(load_vol(data.addr));
+	__dcbst(rdata);
+	__dcbtst(rdata);
+	__dcbf(rdata);
+}
+
+void ppu_reservation_tests()
+{
+	printf("PPU tests on self thread:\n");
+
+	reset_obj(rdata);
+
+	sys_ppu_thread_t help = 0;
+	thread_data_t data = {};
+
+	const struct callback_info_t
+	{
+		void(*cb)(thread_data_t&);
+		const char* name;
+	} calls[5] =
+	{
+		{&change_addr, "change_addr"},
+		{&write_val, "write_val"},
+		{&invalidate_cache, "invalidate_cache"},
+		{&atomic_op, "atomic_op"},
+		{&store_cond, "store_cond"}
+	};
+
+	ENSURE_OK(sys_ppu_thread_create(&help, helper_thread,bit_cast<uptr>(&data),1001, 0x10000,1,"t"));
+
+	static const u32 to_repeat = 500; 
+
+	// Testing reservation modification with different instruction mashups
+	for (u32 i = 0; i < 4; i++)
+	{
+		bool success = false;
+
+		for (u32 j = 0; j < to_repeat && !success; sys_ppu_thread_yield(), j++)
+		{
+			(i & 1) ? __ldarx(rdata) : __lwarx(rdata);
+
+			// In case we lost reservation due to context switch, repeat a few time
+			success |= ((i & 2) ? __stdcx(rdata, 0) : __stwcx(rdata, 0)) != 0;
+		}
+
+		printf("%s load and %s store, success: %d\n", (i & 1) ? "LDARX" : "LWARX", (i & 2) ? "STDCX" : "STWCX", +success);
+	}
+
+	for (u32 i = 0; i < 5/*std::size(calls)*/; i++)
+	{
+		bool success = false;
+
+		for (u32 j = 0; j < to_repeat && !success; sys_ppu_thread_yield(), j++)
+		{
+			store_vol(data.addr, int_cast(&rdata));
+
+			__lwarx(rdata);
+
+			calls[i].cb(data);
+
+			// In case we lost reservation due to context switch, repeat a few time
+			success |= __stwcx(ptr_cast(load_vol(data.addr)), 0);
+		}
+
+		printf("Function '%s' executed on reservation, success: %d\n", calls[i].name, +success);
+	}
+
+	printf("PPU tests with another thread:\n");
+
+	for (u32 i = 0; i < 5/*std::size(calls)*/; i++)
+	{
+		bool success = false;
+
+		for (u32 j = 0; j < to_repeat && !success; sys_ppu_thread_yield(), j++)
+		{
+			store_vol(data.addr, int_cast(&rdata));
+
+			__lwarx(rdata);
+
+			wrap_callback(data, calls[i].cb);
+
+			// In case we lost reservation due to context switch, repeat a few time
+			success |= __stwcx(ptr_cast(load_vol(data.addr)), 0);
+		}
+
+		printf("Function '%s' executed asynchronously on reservation, success: %d\n", calls[i].name, +success);
+	}
+
+	store_vol(data.exit, 1);
+	store_vol(data.signal, 1);
+	ENSURE_VAL(sys_ppu_thread_join(help, NULL), EFAULT);
 	sys_timer_sleep(1);
 } 
 
